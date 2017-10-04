df99be326306202755d03f7c40989a9c316423ce
Melissa Kanhirun <mk7run@gmail.com>
2017-10-04 09:28:15 -0500

    Employee can be created, total sales can be calculated and average of those sales

--- part-1/employee.js
+++ part-1/employee.js
@@ -2,3 +2,20 @@
 /*   It is required to use a for loop                    */
 /*   when iterating over an employee's array of sales.   */
 /*********************************************************/
+var Employee = function(args={}) {
+  this.firstName = args.firstName;
+  this.sales = args.sales;
+};
+
+Employee.prototype.totalSales = function(){
+  var total = 0
+  this.sales.forEach(function(sale){
+    total += sale;
+  });
+  return total;
+};
+
+Employee.prototype.averageSales = function(){
+  var that = this;
+  return (this.totalSales()/this.sales.length)
+};
--- part-1/salesTeam.js
+++ part-1/salesTeam.js
@@ -2,3 +2,8 @@
 /*   It is required to use Array.prototype methods            */
 /*   when iterating over a sales team's array of employees.   */
 /**************************************************************/
+
+var SalesTeam = function(employees){
+  this.employees = employees;
+  var team = employees.concat(employees);
+};
