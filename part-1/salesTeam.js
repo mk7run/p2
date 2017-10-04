8a93acd59481c9018fa31a7baa2f8f4f9f50ca7e
Melissa Kanhirun <mk7run@gmail.com>
2017-10-04 10:00:59 -0500

    save point...can't think

--- part-1/salesTeam.js
+++ part-1/salesTeam.js
@@ -3,7 +3,12 @@
 /*   when iterating over a sales team's array of employees.   */
 /**************************************************************/
 
-var SalesTeam = function(employees){
-  this.employees = employees;
-  var team = employees.concat(employees);
+var SalesTeam = {
+  team: [],
+  addEmployee = function(employee){
+    this.team.push(employee);
+  },
+  createTeam = function(employees) {
+    this.team = this.team.concat(employees);
+  }
 };
