c1a30c7d50f69c257c053086710b2dfb30537be6
Melissa Kanhirun <mk7run@gmail.com>
2017-10-24 16:58:57 -0500

    create employee with inherited function

--- part-1/employee.js
+++ part-1/employee.js
@@ -2,3 +2,20 @@
 /*   It is required to use a for loop                    */
 /*   when iterating over an employee's array of sales.   */
 /*********************************************************/
+
+function Employee(args={}){
+  this.firstName = args.firstName;
+  this.sales = args.sales
+};
+
+Employee.prototype.totalSales = function(){
+  var employee = this;
+
+  for (var i = 0; i < employee.sales.length; i++){
+    return employee.sales.reduce(function(sum, sale){ return sum + sale});
+  };
+};
+
+Employee.prototype.averageSales = function(){
+  return (this.totalSales() / this.sales.length)
+}
